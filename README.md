63a82e1791b2a2e9636c22fca899a6e2d8ef093c
vtoth47 <91272846+vtoth47@users.noreply.github.com>
2021-12-09 08:00:58 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,51 @@
-# test
+# a3web
+Web page for Assignment 3
+https://zacharymasson.github.io/a3web/
 
-Track stuff here.
 
-This will test pushing from VS Code to git
+### Jade
+Location page - create two Static API maps, or create dynamic map using javascript if I can figure it out
+If javascript map is not figured out by Week 12, will move on with Satic API maps
+Write descriptions of company/team location range
+Done by Week 13 lab
+
+### Kerstyne
+Team Page - Mission Statement/About Us: Rough layout/ design (template) done before Week 12 lab
+Animated Social Icons that link to Linkedins, adding actual text and images in by Week 13
+Completion by Week 13 before lab. 
+
+### Ridley
+Portfolio Page - Past and Present projects. Finished by Week 12 Lab. Includes:
+Present Portfolio Items as slideshow carousel - IN PROGRESS
+
+Past Portfolio items as tile gallery of images with descriptions - FINISHED
+
+Responsive styling for both of the above - IN PROGRESS
+
+Anchor links to parts of page from navbar - IN PROGRESS - COMPLETE FOR PORTFOLIO, NOT YET FOR CONTACT
+
+
+
+
+Finished before Week 13:
+
+Landing Page - Image Carousel
+
+Formalize code commenting and review for bugs
+
+### Victor
+Image filter that meshes our photos with our websites colours
+Testimonials:
+
+Create 3 fake testimonials, with images of 'clients' or projects Completion by Week 13 before lab. 
+
+
+Stretch goal - 1 of the fake testimonials is a short video clip, under 10 seconds.
+
+### Zachary
+Join Us/ Contact Us - A form from survey123 that will collect the users name, email, and protect type. Done before week 12 lab.
+
+A map marker that on hover shows contact information per "office" location. On click it will link to an anchor on Jade's Projects page. Done before week 13 lab. 
+
+Stretch goal - When the user completes the survey the colour of the page should change an element on the page to show "We are excited to work with you!"
+
---
+++ js/javascript.js
@@ -0,0 +1,164 @@
+// Modal box for maps on Location page, owner: Jade
+// Code from https://www.w3schools.com/howto/howto_css_modals.asp & https://codepen.io/haydmills/pen/zLraGJ
+
+// Get the modal
+var modalparent = document.getElementsByClassName("modal_multi");
+
+// Get the button that opens the modal
+var modal_btn_multi = document.getElementsByClassName("myBtn_multi");
+
+// Get the <span> element that closes the modal
+var span_close_multi = document.getElementsByClassName("close_multi");
+
+// When the user clicks the button, open the modal
+function setDataIndex() {
+for (i = 0; i < modal_btn_multi.length; i++)
+{
+modal_btn_multi[i].setAttribute('data-index', i);
+modalparent[i].setAttribute('data-index', i);
+span_close_multi[i].setAttribute('data-index', i);
+}
+}
+
+for (i = 0; i < modal_btn_multi.length; i++)
+{
+modal_btn_multi[i].onclick = function() {
+var ElementIndex = this.getAttribute('data-index');
+modalparent[ElementIndex].style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span_close_multi[i].onclick = function() {
+var ElementIndex = this.getAttribute('data-index');
+modalparent[ElementIndex].style.display = "none";
+};
+}
+window.onload = function() {
+setDataIndex();
+};
+
+window.onclick = function(event) {
+if (event.target === modalparent[event.target.getAttribute('data-index')]) {
+modalparent[event.target.getAttribute('data-index')].style.display = "none";
+}
+};
+////////////////////
+// Owner : Ridley Soudack
+// Date  : Dec 6
+////////////////////
+// Mobile Navbar function - based off of: https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+function navBar(){ // This function appends the 'responsive' class to navbar elements when the hamburger menu is clicked, to adjust menu styling
+    var nav = document.getElementById("navbarmaster");
+    if (nav.className === "navbar-menu") {
+        nav.className += " responsive";
+    } else{
+        nav.className = "navbar-menu";
+    }
+    var bar = document.getElementById("headerbar");
+    if (bar.className === "navbar-flex") {
+        bar.className += " responsive";
+    } else {
+        bar.className = "navbar-flex";
+    }
+}
+//Portfolio page slideshow JS based off of: https://www.w3schools.com/howto/howto_js_slideshow.asp 
+var slideIndex = 1; //Sets initial slide to slide 1 when user lands on page
+function showSlides(n) { // This function controls which slide shows up as the variable slide index changes
+  var i; //Set up variable for iteration
+  var slides = document.getElementsByClassName("slide"); //Sets var slides, dots, and blurbs to relevant element classes
+  var dots = document.getElementsByClassName("dot");
+  var blurbs = document.getElementsByClassName("portfolio-blurb");
+  if (n > slides.length) {slideIndex=1} //If n increases beyond slidelength, loop back to slideIndex 1
+  if (n < 1) {slideIndex=slides.length} //If n decreases below 1, loop around to maximum slideIndex.
+  for (i = 0; i < slides.length; i++) { // Set all slides to display = none
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < blurbs.length; i++) { // Set all blurbs to display = none initially
+    blurbs[i].style.display = "none";
+}
+  for (i = 0; i < dots.length; i++) { //Sets active dot styling
+    dots[i].className = dots[i].className.replace(" active", "");
+}
+  slides[slideIndex-1].style.display = "block"; //Set slideIndex to display relevant slide and blurb, and set associated dot to 'active' class.
+  blurbs[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+// Next/previous controls
+function plusSlides(n) { // This function enables control of slides through next and previous button
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) { // This function enables control of slides through the slideshow dots
+  showSlides(slideIndex = n)
+}
+// Button to external application, owner: Ridley
+function appLink() {
+  var myWindow=window.open("https://arcg.is/1uDiH80");
+}
+// End Owner: Shawn Morgan
+// Start Survey123 JS
+var survey123webform = document.getElementsByName('survey123webform')[0];
+window.addEventListener("message",e=>{if(e.data){var t=JSON.parse(e.data)
+  "survey123:onFormLoaded"===t.event&&t.contentHeight&&(survey123webform.parentNode.style["padding-bottom"]="unset")}});
+// Get users location and enter it in url to Google Maps directions - Owner: Zachary
+// gets location from the user and if not allowed in browswer show error message
+// first function is for toronto, second is montreal. 
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(MAPTIME);
+  } else { 
+    x.innerHTML = "Geolocation is not supported :'( ";
+  }
+}
+function getLocation2() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(MAPTIME2);
+  } else { 
+    x.innerHTML = "Geolocation is not supported :'( ";
+  }
+}
+function MAPTIME(position) {
+  var x = position.coords.latitude
+  var y = position.coords.longitude
+  window.open("https://www.google.ca/maps/dir/"+x+","+y+"/CN%2BTower")
+}
+function MAPTIME2(position) {
+  var x = position.coords.latitude
+  var y = position.coords.longitude
+  window.open("https://www.google.ca/maps/dir/"+x+","+y+"/Former%2BCraig%2BPumping%2BStation%2C%2B2000%2BAvenue%2BViger%2BE%2C%2BMontreal%2C%2BQuebec/%4045%2E4817221%2C%2D73%2E6295888%2C12540m")
+}
+// Javascript used to call linkedin pages of each person on teams page Owner: Kerstyne
+function LinkedinKW(){
+  window.open('https://www.linkedin.com/in/kerstyne%2Dwidmeyer%2D733010200');
+}
+function LinkedinJL(){
+  window.open('https://www.linkedin.com/in/jade%2Dlacsamana%2Da7224b131/')
+}
+function LinkedinRS(){
+  window.open('https://www.linkedin.com/in/ridley%2Dsoudack%2D1757081b4/')
+}
+function LinkedinVT(){
+  window.open('https://www.linkedin.com/in/victor%2Dtoth%2Db750172b/')
+}
+function LinkedinZM(){
+  window.open('https://www.linkedin.com/in/zachary%2Dmasson%2D495227198/%3ForiginalSubdomain%3Dca')
+}
+
+// Video player section - Victor Toth
+//javascript used to define the functions of the button for video 1
+
+function playPause() { 
+  var myVideo=document.getElementById("video1"); 
+  if (myVideo.paused) 
+    myVideo.play(); 
+  else 
+    myVideo.pause(); 
+} 
+//javascript used to define the functions of the button for video 2
+
+function playPause2() { 
+  var myVideo2=document.getElementById("video2"); 
+  if (myVideo2.paused) 
+    myVideo2.play(); 
+  else 
+    myVideo2.pause(); 
+} 
